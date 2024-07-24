96320cee791b7906dfada9cc63a5c97be2dc8c5c
lakshu267 <lakshu267@gmail.com>
2024-07-24 09:58:01 +0530

    code modified

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami                    = var.ec2_ami_id
   instance_type          = var.ec2_instance_type
   user_data              = file("apache-install.sh")
-  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  vpc_security_group_ids = [aws_security_group.vpc-ssh-cloud.id, aws_security_group.vpc-web-cloud.id]
   tags = {
     "Name" = "Terraform_Cloud"
   }
