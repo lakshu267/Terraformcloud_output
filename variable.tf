7012e5f70d80f91f2c5f7e39bbd1728894c0b449
lakshu267 <lakshu267@gmail.com>
2024-07-24 10:02:02 +0530

    region ami changed

--- variable.tf
+++ variable.tf
@@ -1,18 +1,18 @@
 # Input Variables
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
-  type = string
-  default = "ap-south-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "ec2_ami_id" {
   description = "AMI ID"
-  type = string  
-  default = "ami-079b5e5b3971bd10d"
+  type        = string
+  default     = "ami-0427090fd1714168b"
 }
 
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
 }
